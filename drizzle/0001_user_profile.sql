70db98932f950644add3139c30a3b97257bd75f0
ai-blip-tech <testaimoblernorr@gmail.com>
2026-09-23 18:40:08 +0300

    Delete drizzle directory

--- drizzle/0001_user_profile.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE users ADD COLUMN first_name TEXT;
-ALTER TABLE users ADD COLUMN last_name TEXT;
-ALTER TABLE users ADD COLUMN phone TEXT;
-ALTER TABLE users ADD COLUMN company_role TEXT;
